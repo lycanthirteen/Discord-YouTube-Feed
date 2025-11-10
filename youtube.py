cf4cdce93e502b21c0215aecaa14a33c3da57bc1
lycanthirteen <lycantrese@gmail.com>
2025-11-10 22:51:01 +0800

    Customize yt.py
    Added my Discord Channel Webhook URL Details

--- youtube.py
+++ youtube.py
@@ -6,7 +6,7 @@ import aiosqlite
 from fake_useragent import UserAgent
 ## note: need to use `poetry shell` in order to access lxml
 
-channel_ids = {'Taylor Swift': 'UCqECaJ8Gagnn7YCbPEzWH6g', 'RecordingAcademy': 'UCq4isO8ZYOZfmvGJ-_1UdIA'}  
+channel_ids = {'Lycanthirteen': 'UCn4PLplqsc-YKUr0amucQCA'}  
 # name of channel is not actually used in the script
 # channel ID's can be found on desktop easily
 # sometimes its in the url, e.g https://www.youtube.com/channel/UCqECaJ8Gagnn7YCbPEzWH6g - UCqECaJ8Gagnn7YCbPEzWH6g is the ID
@@ -19,15 +19,15 @@ ua = UserAgent(platforms='desktop')
 async def create_webhook(title, link, youtube_name, date):
     async with aiohttp.ClientSession() as session:
         # url is the discord webhook url, this allows us to post
-        discord_webhook_url = # enter a webhook url here
+        discord_webhook_url = https://discord.com/api/webhooks/1437449186130919445/kz5Qvzh4xhs71U15J1Vgs6ZHIHtbfW_Uvd2FgrhzwIBrxpXvftkrFgpNm9exxHwyBweJ
         webhook = Webhook.from_url(discord_webhook_url, session=session)
 
         # https://docs.disnake.dev/en/latest/api/webhooks.html#disnake.Webhook.send
-        # username = name of webhook user
+        # username = lycanthirteen
         ## await send(content=..., *, username=..., avatar_url=..., tts=False, ephemeral=..., suppress_embeds=..., flags=..., file=..., files=..., embed=..., embeds=..., allowed_mentions=..., view=..., components=..., thread=..., thread_name=..., applied_tags=..., wait=False, delete_after=..., poll=...)
 
-        webhook_username = YouTube Bot # enter name of the Webhook user, can be anything (in this case, it's YouTube Bot)
-        webhook_avatar = "https://i.imgur.com/22Q9QyJ.png" # enter a url to a pic if you want a avatar
+        webhook_username = Lycanmon YT Feeds # enter name of the Webhook user, can be anything (in this case, it's YouTube Bot)
+        webhook_avatar = "" # enter a url to a pic if you want a avatar
         await webhook.send(content=f"NEW UPLOAD FROM {youtube_name}!\n'{title}'\n{link}\n{date}", username=webhook_username, avatar_url=webhook_avatar)
         await asyncio.sleep(3)
 
